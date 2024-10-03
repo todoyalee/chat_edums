6c38bb2c767283110a733d22370bcd5e00c66794
Mohamed Ali Belkouri <medali.b@devnet.me>
2024-10-03 08:32:52 +0100

    feat:adding_version_of_pop_up

--- lib/views/components/ReclamationCard.dart
+++ lib/views/components/ReclamationCard.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
+
 import 'package:chat_edums/views/components/Reclamation_structure.dart';
 
 class ReclamationCard extends StatefulWidget {
   final Reclamation reclamation;
-
   const ReclamationCard({Key? key, required this.reclamation})
       : super(key: key);
 
@@ -11,176 +11,151 @@ class ReclamationCard extends StatefulWidget {
   _ReclamationCardState createState() => _ReclamationCardState();
 }
 
-class _ReclamationCardState extends State<ReclamationCard> {
+class _ReclamationCardState extends State<ReclamationCard>
+    with TickerProviderStateMixin {
   bool _isExpanded = false;
 
   @override
   Widget build(BuildContext context) {
-    Color borderColor =
-        widget.reclamation.priorite == 'Urgent' ? Colors.red : Colors.green;
-    Color statusColor = widget.reclamation.statut == 'Resolved'
-        ? Colors.green.withOpacity(0.7)
-        : Colors.red.withOpacity(0.3);
-
-    List<String> descriptionWords = widget.reclamation.description.split(' ');
-    bool isLongDescription = descriptionWords.length > 5;
-
-    return Card(
-      color: Colors.white,
-      margin: const EdgeInsets.symmetric(vertical: 8.0),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(15),
-        side: BorderSide(color: borderColor, width: 1),
-      ),
-      elevation: 6,
-      child: Column(
-        children: [
-          if (widget.reclamation.priorite == 'Urgent')
-            Container(
-              height: 8,
-              width: double.infinity,
-              color: Colors.red,
-            ),
-          if (widget.reclamation.priorite == 'Normal')
-            Container(
-              height: 8,
-
-              width: double.infinity,
-              //color: Colors.green,
-            ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Image.asset("assets/schooll.png",
-                    width: 71, height: 80, fit: BoxFit.cover),
-                const SizedBox(width: 14),
-                Column(
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          _isExpanded = !_isExpanded;
+        });
+      },
+      child: AnimatedSize(
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+        child: Card(
+          color: Colors.white,
+          margin: const EdgeInsets.symmetric(vertical: 8.0),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+            side: BorderSide(color: Colors.grey, width: 1),
+          ),
+          elevation: 6,
+          child: Column(
+            children: [
+              if (widget.reclamation.priorite == 'Urgent')
+                Container(
+                  height: 8,
+                  width: double.infinity,
+                  color: Colors.red,
+                ),
+              if (widget.reclamation.priorite == 'Normal')
+                Container(
+                  height: 8,
+                  width: double.infinity,
+                ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Container(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 8, vertical: 4),
-                      decoration: BoxDecoration(
-                        color: widget.reclamation.priorite == 'Urgent'
-                            ? Colors.red
-                            : Colors.green,
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: Text(
-                        ' ${widget.reclamation.priorite}',
-                        style: const TextStyle(
-                            color: Colors.white, fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(width: 18),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    const SizedBox(height: 50),
-                    Text(
-                      ' ${widget.reclamation.sujet}',
-                      style: const TextStyle(
-                          color: Colors.black, fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(height: 10),
-                    AnimatedSize(
-                      duration: const Duration(milliseconds: 300),
-                      curve: Curves.easeInOut,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          if (_isExpanded || !isLongDescription)
-                            ..._buildVerticalText(descriptionWords)
-                          else
-                            Text(
-                              '${descriptionWords.take(2).join(' ')}...',
-                              style: TextStyle(
-                                color: Colors.black.withOpacity(0.3),
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          if (isLongDescription)
-                            TextButton(
-                              onPressed: () {
-                                setState(() {
-                                  _isExpanded = !_isExpanded;
-                                });
-                              },
-                              child: Text(
-                                _isExpanded ? 'Show less' : 'Show more',
-                                style: TextStyle(color: Colors.blue),
-                              ),
-                            ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(width: 6),
-                Column(
-                  children: [
-                    Opacity(
-                      opacity: 0.4,
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 1, vertical: 1),
-                        decoration: BoxDecoration(
-                          color: widget.reclamation.statut == 'Progress'
-                              ? Colors.orange
-                              : widget.reclamation.statut == 'Pending'
-                                  ? Colors.red
-                                  : Colors.green,
-                          borderRadius: BorderRadius.circular(8),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Image.asset(
+                          "assets/schooll.png",
+                          width: 71,
+                          height: 80,
+                          fit: BoxFit.cover,
                         ),
-                        child: Text(
-                          ' ${widget.reclamation.statut}',
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16,
+                        const SizedBox(width: 14),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 8, vertical: 4),
+                              decoration: BoxDecoration(
+                                color: widget.reclamation.priorite == 'Urgent'
+                                    ? Colors.red
+                                    : Colors.green,
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              child: Text(
+                                ' ${widget.reclamation.priorite}',
+                                style: const TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(width: 18),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const SizedBox(height: 8),
+                              Text(
+                                ' ${widget.reclamation.sujet}',
+                                style: const TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              if (_isExpanded) ...[
+                                const SizedBox(height: 10),
+                                Text(
+                                  widget.reclamation.description,
+                                  style: TextStyle(
+                                    color: Colors.black.withOpacity(0.6),
+                                  ),
+                                ),
+                              ]
+                            ],
                           ),
                         ),
-                      ),
+                      ],
                     ),
-                    const SizedBox(height: 50),
-                    const Text(
-                      "By dali ",
-                      style: TextStyle(color: Colors.grey),
-                    ),
+                    if (_isExpanded)
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const SizedBox(height: 10),
+                          Row(
+                            children: [
+                              Opacity(
+                                opacity: 0.4,
+                                child: Container(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 1, vertical: 1),
+                                  decoration: BoxDecoration(
+                                    color: widget.reclamation.statut ==
+                                            'Progress'
+                                        ? Colors.orange
+                                        : widget.reclamation.statut == 'Pending'
+                                            ? Colors.red
+                                            : Colors.green,
+                                    borderRadius: BorderRadius.circular(8),
+                                  ),
+                                  child: Text(
+                                    ' ${widget.reclamation.statut}',
+                                    style: const TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(width: 10),
+                              const Text(
+                                "By dali ",
+                                style: TextStyle(color: Colors.grey),
+                              ),
+                            ],
+                          ),
+                        ],
+                      )
                   ],
                 ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  // Function to build vertical text display
-  List<Widget> _buildVerticalText(List<String> words) {
-    List<Widget> verticalTextWidgets = [];
-
-    // Iterate through the words in pairs
-    for (int i = 0; i < words.length; i += 2) {
-      // Create a line with two words or just one if it's odd
-      String line =
-          (i + 1 < words.length) ? '${words[i]} ${words[i + 1]}' : words[i];
-      verticalTextWidgets.add(
-        Text(
-          line,
-          style: TextStyle(
-            color: Colors.black.withOpacity(0.3),
-            fontWeight: FontWeight.bold,
+              ),
+            ],
           ),
         ),
-      );
-    }
-
-    return verticalTextWidgets;
+      ),
+    );
   }
 }
